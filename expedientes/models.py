ed9f303fffbe80518e0c832bf463e4fc5e906d91
Nayelis3 <s22120217@alumnos.itsur.edu.mx>
2025-09-27 20:39:30 -0600

    Se definieron los modelos de datos Trabajador y Documento y se realizaron las migraciones correspondientes para la base de datos.

--- expedientes/models.py
+++ expedientes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Trabajador(models.Model):
+    nombre = models.CharField(max_length=100)
+    puesto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.puesto}"
+
+class Documento(models.Model):
+    trabajador = models.ForeignKey(
+        Trabajador, on_delete=models.CASCADE, related_name="documentos"
+    )
+    archivo = models.FileField(upload_to="documentos/")
+
+    def __str__(self):
+        return f"Documento de {self.trabajador.nombre}: {self.archivo.name}"
